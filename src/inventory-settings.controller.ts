88693cefc45f21f1828e269ada067519eb867ac0
Abhilash Pillai <abhi2kollam@gmail.com>
2022-03-02 11:17:24 +0530

    id

--- src/inventory-settings.controller.ts
+++ src/inventory-settings.controller.ts
@@ -1,11 +1,11 @@
-/* eslint-disable prettier/prettier */
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { StoreService } from './store.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Controller('inventory-settings')
 export class InventorySettingsController {
     constructor(private readonly store: StoreService) { }
-
+    
     @Get('list')
     getList(): Promise<string[]> {
         return this.store.readInventorySettings();
@@ -13,6 +13,7 @@ export class InventorySettingsController {
     @Post('save')
     async saveSettings(@Body() setting: any): Promise<boolean> {
         const settings = await this.store.readInventorySettings();
+        setting.id = uuidv4();
         settings.push(setting)
         return this.store.writeApplicationStore(settings);
     }
